4072731df1264ad92d9c2c81575870470b1c9bef
rovast <rovast@163.com>
2022-03-29 14:09:52 +0800

    🎨 hw4 part1, sys_brk does not alloc

--- mit6.828/hw4-lazy-page-allocation/sysproc.c
+++ mit6.828/hw4-lazy-page-allocation/sysproc.c
@@ -51,8 +51,14 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // if(growproc(n) < 0)
+  //   return -1;
+
+  // hw4，只增加 proc->sz，但是不实际分配内存。返回老的 addr
+  // Your new sbrk(n) should just increment 
+  // the process's size (myproc()->sz) by n 
+  // and return the old size
+  myproc()->sz += n;
   return addr;
 }
 
