cc33d76b0dbb05135f7c94231d6c1b4d0f90e57d
rovast <rovast@163.com>
2022-08-29 16:17:09 +0800

    🎨 列表 List 操作

---
+++ elixir/exercism.org/elixir/language-list/lib/language_list.ex
@@ -0,0 +1,19 @@
+defmodule LanguageList do
+  def new() do
+    []
+  end
+
+  def add(list, language) do
+    [language | list]
+  end
+
+  def remove([_ | tail]), do: tail
+
+  def first([head | _]), do: head
+
+  def count(list), do: Enum.count(list)
+
+  def functional_list?(list) do
+    "Elixir" in list
+  end
+end
