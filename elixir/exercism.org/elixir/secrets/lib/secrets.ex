a4b698d77c59a6532d0ef517273a3cd414c4a170
rovast <rovast@163.com>
2022-08-29 13:50:09 +0800

    🎨 函数和位操作

---
+++ elixir/exercism.org/elixir/secrets/lib/secrets.ex
@@ -0,0 +1,32 @@
+require Bitwise
+
+defmodule Secrets do
+  def secret_add(secret) do
+    # fn x -> x + secret end
+    &(&1 + secret)
+  end
+
+  def secret_subtract(secret) do
+    &(&1 - secret)
+  end
+
+  def secret_multiply(secret) do
+    &(&1 * secret)
+  end
+
+  def secret_divide(secret) do
+    &trunc(&1 / secret)
+  end
+
+  def secret_and(secret) do
+    fn x -> Bitwise.band(x, secret) end
+  end
+
+  def secret_xor(secret) do
+    &Bitwise.bxor(&1, secret)
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    &secret_function2.(secret_function1.(&1))
+  end
+end
