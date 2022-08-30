693e8c68e9a0a82f89a11582d8f8ed3b9b570839
rovast <rovast@163.com>
2022-08-30 13:57:37 +0800

    🎨 charlist 操作

---
+++ elixir/exercism.org/elixir/german-sysadmin/lib/username.ex
@@ -0,0 +1,18 @@
+defmodule Username do
+  def sanitize(''), do: ''
+
+  def sanitize([head | tail]) do
+    sanitized =
+      case head do
+        ?ß -> 'ss'
+        ?ä -> 'ae'
+        ?ö -> 'oe'
+        ?ü -> 'ue'
+        x when x >= ?a and x <= ?z -> [x]
+        ?_ -> '_'
+        _ -> ''
+      end
+
+    sanitized ++ sanitize(tail)
+  end
+end
