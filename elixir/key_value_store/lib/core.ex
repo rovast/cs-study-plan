1617e402d87a1d0a0b748cfa42b98d930cc5b989
rovast <rovast@163.com>
2022-08-30 16:03:16 +0800

    🎨 进程和通信的 demo

---
+++ elixir/key_value_store/lib/core.ex
@@ -0,0 +1,9 @@
+defmodule KeyValueStore.Core do
+  def new, do: %{}
+
+  def add_or_update(store, key, value), do: Map.put(store, key, value)
+
+  def delete(store, key), do: Map.delete(store, key)
+
+  def retrive(store, key), do: Map.get(store, key)
+end
