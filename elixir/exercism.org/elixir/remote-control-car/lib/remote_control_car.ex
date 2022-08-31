5572129603c62f14de932e0fa3bc2765668035f1
rovast <rovast@163.com>
2022-08-31 10:57:44 +0800

    🎨 结构体 struct 的操作

--- elixir/exercism.org/elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/exercism.org/elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,23 +1,48 @@
 defmodule RemoteControlCar do
-  # Please implement the struct with the specified fields
+  @enforce_keys [:nickname]
+  defstruct [
+    :nickname,
+    battery_percentage: 100,
+    distance_driven_in_meters: 0
+  ]
 
-  def new() do
-    # Please implement the new/0 function
+  def new(nickname \\ "none") do
+    # %RemoteControlCar{nickname: nickname}
+    %__MODULE__{nickname: nickname}
   end
 
-  def new(nickname) do
-    # Please implement the new/1 function
+  def display_distance(%__MODULE__{} = remote_car) do
+    "#{remote_car.distance_driven_in_meters} meters"
   end
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
+  def display_battery(%__MODULE__{} = remote_car) do
+    case remote_car.battery_percentage do
+      0 -> "Battery empty"
+      n -> "Battery at #{n}%"
+    end
   end
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
-  end
+  def drive(%__MODULE__{} = remote_car) do
+    case remote_car.battery_percentage do
+      0 ->
+        remote_car
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+      n ->
+        %{
+          remote_car
+          | battery_percentage: n - 1,
+            distance_driven_in_meters: remote_car.distance_driven_in_meters + 20
+        }
+    end
+
+    # cond do
+    #   remote_car.battery_percentage > 0 ->
+    #     remote_car
+    #     |> Map.put(:distance_driven_in_meters, remote_car.distance_driven_in_meters + 20)
+    #     |> Map.put(:battery_percentage, remote_car.battery_percentage - 1)
+
+    #   true ->
+    #     remote_car
+    # end
   end
 end
