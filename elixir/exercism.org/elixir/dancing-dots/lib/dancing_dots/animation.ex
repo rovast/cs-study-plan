8633c909cbc25222a165419d73bb832d9f90fff6
rovast <rovast@163.com>
2022-08-31 15:33:04 +0800

    🎨 behavior 类似接口契约的操作

---
+++ elixir/exercism.org/elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -0,0 +1,53 @@
+defmodule DancingDots.Animation do
+  @type dot :: DancingDots.Dot.t()
+  @type opts :: keyword
+  @type error :: any
+  @type frame_number :: pos_integer
+
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
+end
+
+defmodule DancingDots.Flicker do
+  use DancingDots.Animation
+  alias DancingDots.Dot
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, _opts) do
+    case rem(frame_number, 4) do
+      0 -> %Dot{dot | opacity: dot.opacity / 2}
+      _ -> dot
+    end
+  end
+end
+
+defmodule DancingDots.Zoom do
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init([velocity: value] = opts) when is_integer(value) do
+    {:ok, opts}
+  end
+
+  def init(velocity: value),
+    do:
+      {:error,
+       "The :velocity option is required, and its value must be a number. Got: #{inspect(value)}"}
+
+  def init(_) do
+    {:error, "The :velocity option is required, and its value must be a number. Got: nil"}
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    %DancingDots.Dot{dot | radius: dot.radius + (frame_number - 1) * opts[:velocity]}
+  end
+end
