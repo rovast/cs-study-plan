b3ff846612d34e326757ef18d0734a3a97b16521
rovast <rovast@163.com>
2022-09-02 18:11:49 +0800

    🎨 OTP demo

---
+++ elixir/super_duper/lib/super_duper/application.ex
@@ -0,0 +1,30 @@
+defmodule SuperDuper.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  alias SuperDuper.Server
+
+  @impl true
+  def start(_type, _args) do
+    IO.puts(">>>> Starting Super-duper Super-visor <<<<")
+
+    # children = [
+    #   %{id: :superdave, start: {Server, :start_link, [:superdave]}},
+    #   %{id: :superman, start: {Server, :start_link, [:superman]}},
+    #   %{id: :supermario, start: {Server, :start_link, [:supermario]}}
+    # ]
+    children = [
+      {Server, :superdave},
+      {Server, :superman},
+      {Server, :supermario}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_all, name: SuperDuper.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
