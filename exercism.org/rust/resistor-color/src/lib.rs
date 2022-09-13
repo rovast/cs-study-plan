91d0481207670890f2b0d2b69eafbfbae456dbf9
rovast <rovast@163.com>
2022-09-13 17:37:19 +0800

    🎨 crate

---
+++ exercism.org/rust/resistor-color/src/lib.rs
@@ -0,0 +1,28 @@
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+#[derive(Debug, PartialEq, Copy, Clone, IntoEnumIterator, IntEnum)]
+#[repr(usize)]
+pub enum ResistorColor {
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
+}
+pub fn color_to_value(_color: ResistorColor) -> usize {
+    _color as usize
+}
+pub fn value_to_color_string(value: usize) -> String {
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string()
+    }
+}
+pub fn colors() -> Vec<ResistorColor> {
+    ResistorColor::into_enum_iter().collect()
+}
