72afb038bb9783b29e0a4b79de3c34d4b6c8906c
rovast <rovast@163.com>
2022-09-22 16:30:07 +0800

    🎨 完成 kvcounter example

--- wasmcloud/kvcounter/src/lib.rs
+++ wasmcloud/kvcounter/src/lib.rs
@@ -1,5 +1,7 @@
+use serde_json::json;
 use wasmbus_rpc::actor::prelude::*;
 use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, HttpServerReceiver};
+use wasmcloud_interface_keyvalue::{IncrementRequest, KeyValue, KeyValueSender};
 
 #[derive(Debug, Default, Actor, HealthResponder)]
 #[services(Actor, HttpServer)]
@@ -8,24 +10,41 @@ struct KvcounterActor {}
 /// Implementation of HttpServer trait methods
 #[async_trait]
 impl HttpServer for KvcounterActor {
-
     /// Returns a greeting, "Hello World", in the response body.
     /// If the request contains a query parameter 'name=NAME', the
     /// response is changed to "Hello NAME"
     async fn handle_request(
         &self,
-        _ctx: &Context,
+        ctx: &Context,
         req: &HttpRequest,
     ) -> std::result::Result<HttpResponse, RpcError> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
+        let key = format!("counter:{}", req.path.replace('/', ":"));
 
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
+        let amount: i32 = form_urlencoded::parse(req.query_string.as_bytes())
+            .find(|(n, _)| n == "amount")
+            .map(|(_, v)| v.parse::<i32>())
+            .unwrap_or(Ok(1))
+            .unwrap_or(1);
+
+        let (body, status_code) = match increment_counter(ctx, key, amount).await {
+            Ok(v) => (json!({ "counter": v }).to_string(), 200),
+            Err(e) => (json!({ "error": e.to_string() }).to_string(), 500),
+        };
+
+        let resp = HttpResponse {
+            body: body.as_bytes().to_vec(),
+            status_code,
             ..Default::default()
-        })
+        };
+
+        Ok(resp)
     }
 }
 
+/// increment the counter by the amount, returning the new value
+async fn increment_counter(ctx: &Context, key: String, value: i32) -> RpcResult<i32> {
+    let new_val = KeyValueSender::new()
+        .increment(ctx, &IncrementRequest { key, value })
+        .await?;
+    Ok(new_val)
+}
